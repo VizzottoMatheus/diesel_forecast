38dd5f48999d035fe97a9eb54e02451a3e307743
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2022-03-21 22:22:06 -0300

    eda

--- eda.R
+++ eda.R
@@ -1,15 +1,22 @@
 library(lubridate)
 library(TSstudio)
 library(ggplot2)
+library(dplyr)
 
 
-
-# DADOS
+############## EXTRAÇÃO DOS DADOS
 source("get_data.R")
 df_rs <- oil_data_state()
-
 head(df_rs)
 tail(df_rs)
+
+
+############## PREPARAÇÃO DOS DADOS
+df_rs$ANO <- year(df_rs$ANO_MES)
+df_rs_anual <- group_by(df_rs, ANO) %>% 
+                  summarise(
+                          vendas = sum(VENDAS)
+                            )
 
 date_min_y <- year(min(df_rs$ANO_MES))
 date_min_m <- month(min(df_rs$ANO_MES))
@@ -19,41 +26,52 @@ date_max_y <- year(max(df_rs$ANO_MES))
 date_max_m <- month(max(df_rs$ANO_MES))
 date_max <- c(date_max_y, date_max_m)
 
+
+# SÉRIE ANUAL
+diesel_ts_year <- ts(df_rs_anual$vendas, start = date_min_y, end = date_max_y -1, frequency = 1)
+ts_info(diesel_ts_year)
+
+# SÉRIE MENSAL
 diesel_ts <- ts(df_rs$VENDAS, start = date_min, end = date_max, frequency = 12)
-diesel_ts_log <- ts(log(df_rs$VENDAS), start = date_min, end = date_max, frequency = 12)
 ts_info(diesel_ts)
 
-ts_plot(diesel_ts,
+# SÉRIE MENSAL EM LOGARITMO
+diesel_ts_log <- ts(log(df_rs$VENDAS), start = date_min, end = date_max, frequency = 12)
+ts_info(diesel_ts_log)
+
+ts_info(diesel_ts)
+
+
+############## VISUALIZAÇÃO
+
+# SÉRIE ANUAL
+ts_plot(diesel_ts_year,
         color = "green",
         Ygrid = TRUE,
         Xgrid = TRUE,
-        title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
+        title = "VENDAS ANUAIS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
 
-ts_plot(log(diesel_ts),
-        color = "green",
-        Ygrid = TRUE,
-        Xgrid = TRUE,
-        title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
 
-trend <- rollapply(diesel_ts, width = 12, FUN = mean)
+# SÉRIE MENSAL COM RENDÊNCIA
+trend <- rollapply(diesel_ts, width = 12, FUN = mean) #OPÇÃO 1
+#trend <- decompose(diesel_ts)$trend # OPÇÃO 2
 df <- cbind(diesel_ts, trend)
-colnames(df) <- c("VENDAS", "MÉDIA MÓVEL 12 MESES")
+colnames(df) <- c("VENDAS MENSAIS", "MÉDIA MÓVEL 12 MESES")
 ts_plot(df,
         color = "green",
         Ygrid = TRUE,
         Xgrid = TRUE,
         title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
 
+# SÉRIE EM LOGARITMO
 ts_plot(log(df),
         color = "green",
         Ygrid = TRUE,
         Xgrid = TRUE,
         title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
 
-dec <- decompose(diesel_ts)
-plot(dec)
 
-### ANÁLISE DE SAZONALIDADE
+############## ANÁLISE DE SAZONALIDADE
 
 ggseasonplot(diesel_ts, continuous = TRUE) + 
   theme_bw() +
@@ -65,8 +83,8 @@ ggsubseriesplot(diesel_ts) +
   scale_y_continuous(labels = scales::number) +
   labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL")
 
+# SÉRIE DIFERENCIADA
 diesel_ts_diff <- diff(diesel_ts) # cria série diferenciada
-
 ggseasonplot(diesel_ts_diff) + 
   theme_bw() +
   scale_y_continuous(labels = scales::number) +
@@ -77,8 +95,15 @@ ggsubseriesplot(diesel_ts_diff) +
   scale_y_continuous(labels = scales::number) +
   labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL (SÉRIE DIFERENCIADA")
 
+# SIGNIFICÂNCIA DA SAZONALIDADE MENSAL
 ts_seasonal(diesel_ts, type = "box")
-ts_seasonal(diesel_ts_diff, type = "box")
+reg_seas <- tslm(diesel_ts ~ season) # MESES SÃO SIGNIFICATIVOS?
+summary(reg_seas)
 
+ts_seasonal(diesel_ts_diff, type = "box")
+reg_seas <- tslm(diesel_ts_diff ~ season) # MESES SÃO SIGNIFICATIVOS?
+summary(reg_seas)
+
+ts_seasonal(diesel_ts_log, type = "box")
 reg_seas <- tslm(diesel_ts_log ~ season) # MESES SÃO SIGNIFICATIVOS?
 summary(reg_seas)
--- time_series_analysis.R
+++ time_series_analysis.R
@@ -11,9 +11,6 @@ library(plotly)
 source("get_data.R")
 df_rs <- oil_data_state() 
 
-head(df_rs)
-tail(df_rs)
-
 ### OBJETO TIME SERIES
 
 date_min_y <- year(min(df_rs$ANO_MES))
@@ -27,51 +24,6 @@ date_max <- c(date_max_y, date_max_m)
 diesel_ts <- ts(df_rs$VENDAS, start = date_min, end = date_max, frequency = 12)
 diesel_ts_log <- ts(log(df_rs$VENDAS), start = date_min, end = date_max, frequency = 12)
 ts_info(diesel_ts)
-
-
-### ANÁLISE DE SAZONALIDADE
-
-ggseasonplot(diesel_ts, continuous = TRUE) + 
-  theme_bw() +
-  scale_y_continuous(labels = scales::number) +
-  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL")
-
-ggsubseriesplot(diesel_ts) +
-  theme_bw() +
-  scale_y_continuous(labels = scales::number) +
-  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL")
-
-diesel_ts_diff <- diff(diesel_ts) # cria série diferenciada
-
-ggseasonplot(diesel_ts_diff) + 
-  theme_bw() +
-  scale_y_continuous(labels = scales::number) +
-  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL (SÉRIE DIFERENCIADA")
-
-ggsubseriesplot(diesel_ts_diff) +
-  theme_bw() +
-  scale_y_continuous(labels = scales::number) +
-  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL (SÉRIE DIFERENCIADA")
-
-reg_seas <- tslm(diesel_ts_log ~ season) # MESES SÃO SIGNIFICATIVOS?
-summary(reg_seas) # JAN, MAR, ABRI, MAI, AGO, SET, OUT, NOV
-
-
-######### QUEBRA ESTRUTURAL
-#model1 <- Fstats(df_rs$VENDAS~1, from = 0.01)
-#sctest(model1)
-#quebras <- breakpoints(df_rs$VENDAS~1)
-#quebras
-# DATAS COM POSSÍVEL QUEBRA ESTRUTURAL
-#quebras <- quebras$breakpoints
-#dates <- as.Date(df_rs[quebras,1])
-#values <- df_rs[quebras,2]
-#df_bp <- as.data.frame(cbind(dates, values))
-#df_bp$dates <- as.Date(df_bp$dates)
-#ggplot(df_rs, aes(x = ANO_MES, y = VENDAS)) +
-#  geom_line() +
-#  geom_point(data = df_bp, aes(x = dates, y = values), col = "red", size = 2)
-
 
 
 ######### MODELOS #########
